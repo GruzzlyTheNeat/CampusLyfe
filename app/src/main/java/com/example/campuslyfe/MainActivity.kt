c1681ecde1a5564f8b2e2d03be05aa2ef4f409c2
GruzzlyTheNeat <gnshchll@hotmail.com>
2022-04-05 21:15:44 +0300

    sa

--- app/src/main/java/com/example/campuslyfe/MainActivity.kt
+++ app/src/main/java/com/example/campuslyfe/MainActivity.kt
@@ -1,26 +1,43 @@
 package com.example.campuslyfe
 
-import android.R
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.example.campuslyfe.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 
-
-private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var binding: ActivityMainBinding
+    val navController: NavController by lazy { findNavController(R.id.fragmentContainerView2) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         val view = binding.root
         setContentView(view)
+        binding.bottomNavigation.setupWithNavController(navController)
+        binding.bottomNavigation.setOnItemReselectedListener {
+            when(it.itemId){
+                R.id.mainFragment ->{
+                    navController.navigate(R.id.mainFragment)
+                }
+                R.id.profilFragment -> {
+                    navController.navigate(R.id.profilFragment)
+                }
+                R.id.ayarlarFragment ->{
+                    navController.navigate(R.id.ayarlarFragment)
+                }
+            }
+        }
 
 
 
