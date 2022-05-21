60a5e802de8d9765ae1f9a1fb52d0fd174a3eec2
farfalletonde <haybak21@gmail.com>
2022-05-21 16:03:09 +0300

    klavyede okey tuşuna basınca giriş yap / kayıt ol tuşuna basmış gibi davran

--- app/src/main/java/com/example/campuslyfe/fragment/signIn/SignInPasswordFragment.kt
+++ app/src/main/java/com/example/campuslyfe/fragment/signIn/SignInPasswordFragment.kt
@@ -2,6 +2,7 @@ package com.example.campuslyfe.fragment.signIn
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -29,7 +30,9 @@ class SignInPasswordFragment : Fragment() {
 
         binding.lifecycleOwner = viewLifecycleOwner
         binding.viewModel = signInSignUpViewModel
-
+        binding.eTextSighInPassword.setOnEditorActionListener { _, _, _ ->
+            binding.buttonDevamEtPassword.performClick()
+        }
         binding.buttonDevamEtPassword.setOnClickListener {
             val eMail: String = signInSignUpViewModel.email.value?.trim()!!
             val password: String? = signInSignUpViewModel.password.value?.trim()
