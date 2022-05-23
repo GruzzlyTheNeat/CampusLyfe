dfec35ad90ca7cb5076c20d99f617a9cc0d25893
farfalletonde <haybak21@gmail.com>
2022-05-23 20:59:30 +0300

    splash screen eklendi

--- app/src/main/java/com/example/campuslyfe/activity/SignInActivity.kt
+++ app/src/main/java/com/example/campuslyfe/activity/SignInActivity.kt
@@ -2,14 +2,14 @@ package com.example.campuslyfe.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.campuslyfe.R
-import com.example.campuslyfe.databinding.ActivityMainBinding
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.campuslyfe.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         binding = ActivitySignInBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
