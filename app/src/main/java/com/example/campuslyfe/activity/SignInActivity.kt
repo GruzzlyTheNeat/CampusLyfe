5349226a9b9bfc7d7c1bc25fe7ac2425112160d6
farfalletonde <haybak21@gmail.com>
2022-05-23 21:06:49 +0300

    persistent login

--- app/src/main/java/com/example/campuslyfe/activity/SignInActivity.kt
+++ app/src/main/java/com/example/campuslyfe/activity/SignInActivity.kt
@@ -1,15 +1,22 @@
 package com.example.campuslyfe.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.campuslyfe.databinding.ActivitySignInBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignInActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
+        if (Firebase.auth.currentUser != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
         binding = ActivitySignInBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
