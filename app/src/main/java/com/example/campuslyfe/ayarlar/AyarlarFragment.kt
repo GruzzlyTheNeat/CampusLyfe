70f9f984e0304af4689ef407df0710ccbb10b6b2
farfalletonde <haybak21@gmail.com>
2022-05-25 09:21:43 +0300

    log out

--- app/src/main/java/com/example/campuslyfe/ayarlar/AyarlarFragment.kt
+++ app/src/main/java/com/example/campuslyfe/ayarlar/AyarlarFragment.kt
@@ -1,5 +1,6 @@
 package com.example.campuslyfe.ayarlar
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,28 +8,28 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.campuslyfe.R
+import com.example.campuslyfe.activity.SignInActivity
 import com.example.campuslyfe.databinding.FragmentAyarlarBinding
-import com.example.campuslyfe.databinding.FragmentMainBinding
-import com.example.campuslyfe.databinding.FragmentProfilBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class AyarlarFragment : Fragment() {
-    private lateinit var binding : FragmentAyarlarBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         val binding = FragmentAyarlarBinding.inflate(inflater, container, false)
 
 
         binding.cardViewProfilGuncelle.setOnClickListener {
             findNavController().navigate(R.id.action_ayarlarFragment_to_profilUpdateFragment)
-
         }
         binding.cardViewCikisYap.setOnClickListener {
-
-
+            Firebase.auth.signOut()
+            startActivity(Intent(requireContext(), SignInActivity::class.java))
+            activity?.finish()
         }
         binding.cardViewProgramTanitimi.setOnClickListener {
 
